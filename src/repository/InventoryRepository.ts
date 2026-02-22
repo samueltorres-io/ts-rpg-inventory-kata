d801e85c0c141f7488d55f0a5ac61ee8e46c5be1
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 15:58:58 -0300

    Add methods to InventoryRepository for managing user inventories: get, set, and delete

--- src/repository/InventoryRepository.ts
+++ src/repository/InventoryRepository.ts
@@ -1,8 +1,19 @@
 import { Inventory } from '../entity/Inventory';
-import { User } from '../entity/User';
 
 export class InventoryRepository {
 
     private inventories = new Map<number, Inventory>();
+
+    public getInventory(userId: number) {
+        return this.inventories.get(userId);
+    }
+
+    public setInventory(userId: number, inventory: Inventory) {
+        return this.inventories.set(userId, inventory);
+    }
+
+    public deleteInventory(userId: number, inventory: Inventory) {
+        return this.inventories.delete(userId);
+    }
 
 }
