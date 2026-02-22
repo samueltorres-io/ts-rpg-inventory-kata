0922e0e7ae0976352e7c9e313e313020407b392a
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 17:49:28 -0300

    Remove inventoryMaxSlots property from User class and update constructor accordingly. Slots são um atributo de Inventory relacionado ao user, não de user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,11 +1,9 @@
 export class User {
     id: number;
     name: string;
-    inventoryMaxSlots: number;
 
     constructor(id: number, name: string, slots: number) {
         this.id = id;
         this.name = name;
-        this.inventoryMaxSlots = slots;
     }
 }
