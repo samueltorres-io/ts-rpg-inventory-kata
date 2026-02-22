99dcec0979538feb844223a36ef2cde75a3dc4c1
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 17:58:15 -0300

    Adding a basic logic of this function

--- src/service/inventoryService.ts
+++ src/service/inventoryService.ts
@@ -1,12 +1,15 @@
 import { User } from "../entity/User";
 import { Inventory } from "../entity/Inventory";
+import { Item } from '../entity/Item';
 import { InventoryRepository } from '../repository/InventoryRepository';
 import { UserRepository } from '../repository/UserRepository';
+import { ItemRepository } from '../repository/ItemRepository';
 
 export class InventoryService {
 
     private inventoryRepository: InventoryRepository;
     private userRepository: UserRepository;
+    private itemRepository: ItemRepository;
 
     /* Criar um novo inventario para um user */
     public create(user: User, inventory: Inventory) {
@@ -31,6 +34,50 @@ export class InventoryService {
         }
         /* Vai que user tem mais de um inventário.... excluimos apenas o inventário exato do user */
         return this.inventoryRepository.deleteInventory(user.id, inventory);
+    }
+
+    /* Adicionar um item ao inventário do user */
+    public addItem(user: User, item: Item) {
+        if (!this.userRepository.getUser(user.id)) {
+            return new Error("User not found");
+        }
+        if (!this.itemRepository.getItem(item.id)) {
+            return new Error("Item not found");
+        }
+        
+        const userInventory = this.getInventory(user);
+
+        /**
+         * Com o userInvntory, iremos validar quantos slots ele 
+         * tem vazios de acordo com o slots, além de validar se
+         * o user tem peço que não esceda o máximo ermitido e se
+         * o novo item caberia e tem peso o sufucuente para não
+         * vazar isso!
+        */
+
+        /*
+        export class Inventory {
+            slots: number;
+            weight: number;
+            // item ID | amount
+            items: Map<number, number>;
+        }
+        */
+
+        /**
+         * Para cada item dentro de items, vamos pegar o amount
+         * e ir somando todos os items para ver se bate sendo 
+         * menor ou igual ao slots, pois cada item:number é
+         * um slot e cada item:id devemos buscar no items pelo
+         * id e verificar o peso dele, então: (busca item -> puxa
+         * peso -> verifica amount -> peso x amount -> salva e vai
+         * para o proximo item)!
+         * 
+         * Ou lidar com item sendo um slot e podendo fazer um stack de items x em um slot com quantidade maxima de items por stack/slot
+         * Tipo, armaduras/armas (Armor/Weapon) não estacam, mas outros items estacam em x vezes.
+        */
+        
+
     }
 
 }
