453cec5cbaab01a1bea30bcaf4e10fc787a93415
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-23 17:42:56 -0300

    Update Inventory class to allow items to be null, enhancing flexibility in item management.

--- src/entity/Inventory.ts
+++ src/entity/Inventory.ts
@@ -2,12 +2,12 @@ export class Inventory {
     slots: number;
     weight: number;
     /* item ID | amount */
-    items: Map<number, number>;
+    items: Map<number, number> | null;
 
     constructor(
         slots: number,
         weight: number,
-        items: Map<number, number>
+        items: Map<number, number> | null
     ) {
         this.slots = slots;
         this.weight = weight;
