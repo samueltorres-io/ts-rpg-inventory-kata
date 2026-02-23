1612241b615f494635f42bb6263ab61ec7035ad6
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 22:32:08 -0300

    Add constructor to Item class to initialize properties including id, name, type, durability, weight, and maxStack for improved object instantiation.

--- src/entity/Item.ts
+++ src/entity/Item.ts
@@ -5,4 +5,20 @@ export class Item {
     durability: number;
     weight: number;
     maxStack: number;
+
+    constructor(
+        id: number,
+        name: string,
+        type: object,
+        durability: number,
+        weight: number,
+        maxStack: number
+    ) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.durability = durability;
+        this.weight = weight;
+        this.maxStack = maxStack;
+    }
 }
