41fecef6e3d57cdc7c98b4219f98f78389071431
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 22:32:19 -0300

    Add constructor to Weapon class to initialize attack property and update import statement for compatibility with .js extension.

--- src/entity/Weapon.ts
+++ src/entity/Weapon.ts
@@ -1,5 +1,18 @@
-import { Item } from "./Item";
+import { Item } from "./Item.js";
 
 export class Weapon extends Item {
     attack: number;
+
+    constructor(
+        id: number,
+        name: string,
+        type: object,
+        durability: number,
+        weight: number,
+        maxStack: number,
+        attack: number
+    ) {
+        super(id, name, type, durability, weight, maxStack),
+        this.attack = attack;
+    }
 }
