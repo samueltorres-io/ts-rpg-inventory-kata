c06832e987b315d6833839517e5b08121463d080
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 22:32:03 -0300

    Update Armor class constructor to include protection parameter and change import statement to use .js extension for compatibility

--- src/entity/Armor.ts
+++ src/entity/Armor.ts
@@ -1,5 +1,18 @@
-import { Item } from "./Item";
+import { Item } from "./Item.js";
 
 export class Armor extends Item {
     protection: number;
+
+    constructor(
+        id: number,
+        name: string,
+        type: object,
+        durability: number,
+        weight: number,
+        maxStack: number,
+        protection: number
+    ) {
+        super(id, name, type, durability, weight, maxStack),
+        this.protection = protection;
+    }
 }
