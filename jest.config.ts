5860e48b45c804071e54d4fa66bbb1bd9c3e3af8
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-23 16:56:37 -0300

    Add moduleNameMapper in jest.config.ts to handle .js extensions in imports, ensuring compatibility with ES modules.

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
