03339a02ea518caab89ad8b662b6ba5793c235ac
samueltorres-io <torresfernandessamuel@gmail.com>
2026-02-22 22:31:54 -0300

    Replace jest.config.js with jest.config.ts for improved TypeScript support and ESM compatibility; update package.json for module type and test script; enhance tsconfig.json with module resolution and stricter settings.

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
+
+export default jestConfig
